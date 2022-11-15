68e5849b0eb7cc30686863725a66e803ec838491
nishantkumar1308 <nishantsingh1308@gmail.com>
2022-11-15 13:21:21 +0530

    Adding Navbar component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import "./App.css";
+
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 import Card from "./Components/Card";
 import Form from "./Components/Form";
+import NavBar from "./Components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <Form />
+      <NavBar />
     </div>
   );
 }
